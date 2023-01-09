b81e53f77d1b00ad2f8f0c104ba46f23569eb33b
WessellUrdata <86524864+WessellUrdata@users.noreply.github.com>
2023-01-09 21:26:17 +0800

    Updated Lab1A

--- Lab1-A/src/main.c
+++ Lab1-A/src/main.c
@@ -1,10 +1,8 @@
 #include <avr/io.h>
 
-int main() {
-
 /*
 Clock config:
-Compare Match Output A: 10: Clear OC0A on compare match, set OC0A at BOTTOM, (non-inverting mode)
+Compare Match Output A: 00: Normal port operation, OC0A disconnected
 Compare Match Output B: 10: Clear OC0B on compare match, set OC0B at BOTTOM, (non-inverting mode)
 Waveform Generation Mode: 111: Mode 7, Fast PWM
 Clock Select: 100: clk/256 (from prescaler)
@@ -12,14 +10,18 @@ Clock Select: 100: clk/256 (from prescaler)
 1/16,000,000 * 256 (prescaler) * 125 (OCR0A) = 0.002s = 500Hz
 */
 
-  TCCR0A = (0b10 << COM0A0) |
-          (0b10 << COM0B0) |
-          (0b11 << WGM00);  
-  TCCR0B = (0b1 << WGM02) |
-          (0b101 << CS00);
+int main() {
 
-  OCR0A = 125 - 1; // 500Hz
-  OCR0B = 63 - 1; // 50% duty cycle
+  TCCR0A =
+        (0b00 << COM0A0) |
+        (0b10 << COM0B0) |
+        (0b11 << WGM00);  
+  TCCR0B =
+        (0b1 << WGM02) |
+        (0b100 << CS00);
+
+  OCR0A = 125-1; // 500Hz (OCR0A = TOP)
+  OCR0B = (125/2)-1; // 50% duty cycle (Update of OCR0B at BOTTOM)
 
   DDRD = (1 << PD5); // PD5 (OC0B)
 
