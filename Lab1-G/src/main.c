683b113c49eaac87fbba80bf7b7291b93e9368fb
WessellUrdata <86524864+WessellUrdata@users.noreply.github.com>
2023-02-06 16:51:54 +0800

    Lab1-G completed

--- Lab1-G/src/main.c
+++ Lab1-G/src/main.c
@@ -1,20 +1,21 @@
 #include <avr/io.h>
-
-/*
-Clock config:
-Compare Match Output A: 00: Normal port operation, OCxA disconnected
-Compare Match Output B: 10: Clear OCxA on compare match, set OCxB at BOTTOM, (non-inverting mode)
-Waveform Generation Mode: 111: Mode 7, Fast PWM / toggle WGM13 in Timer1 for the equivalent mode (Mode 15)
-Clock Select: 100: clk/256 (from prescaler) / 110 in Timer2 for equivalent setting
-
-1/16,000,000 * 256 (prescaler) * 125 (OCRxA) = 0.002s = 500Hz
-
-Red = Timer0 = OC0B = PD5
-Green = Timer1 = OC1B = PB2
-Blue = Timer2 = OC2B = PD3
-*/
+#include <avr/interrupt.h>
 
 void TimersSetup() {
+
+	/*
+	Clock config:
+	Compare Match Output A: 00: Normal port operation, OCxA disconnected
+	Compare Match Output B: 10: Clear OCxA on compare match, set OCxB at BOTTOM, (non-inverting mode)
+	Waveform Generation Mode: 111: Mode 7, Fast PWM / toggle WGM13 in Timer1 for equivalent mode (Mode 15)
+	Clock Select: 100: clk/256 (from prescaler) / 110 in Timer2 for equivalent setting
+
+	1/16,000,000 * 256 (prescaler) * 125 (OCRxA) = 0.002s = 500Hz
+
+	Red = Timer0 = OC0B = PD5
+	Green = Timer1 = OC1B = PB2
+	Blue = Timer2 = OC2B = PD3
+	*/
 
 	TCCR0A = TCCR1A = TCCR2A = (1 << COM0B1) | (0b11 << WGM00); // common settings
 	TCCR0B = TCCR1B = TCCR2B = (0b11 << WGM02) | (0b100 << CS00); // common settings
@@ -27,29 +28,50 @@ void TimersSetup() {
 
 }
 
+void ADCSetup() {
+	// AREF is hooked up to 5V
+	ADCSRA = (1 << ADEN) | (1 << ADIE) | (0b111 << ADPS0); // enable ADC, ADC Interrupt and set prescaler to 128
+	ADCSRA |= (1 << ADSC);
+}
+
+enum LED {
+	RED,
+	GREEN,
+	BLUE
+};
+
+volatile enum LED currentLED = RED;
+ISR(ADC_vect) {
+
+	uint8_t OCRvalue = (ADC > 400) ? 125 : 0; // threshhold at 300, max is around 900 for me
+
+	switch (currentLED) {
+		case RED: 
+			OCR0B = OCRvalue;
+			ADMUX = currentLED = GREEN;
+			break;
+		case GREEN:
+			OCR1B = OCRvalue;
+			ADMUX = currentLED = BLUE;
+			break;
+		case BLUE:
+			OCR2B = OCRvalue;
+			ADMUX = currentLED = RED;
+			break;
+	}
+
+	ADCSRA |= (1 << ADSC);
+
+}
+
+
 int main() {
 
 	TimersSetup();
+	ADCSetup();
+	sei();
 
 	while (1) {
-		for (int i = 1; i < 125 * 262; i++) {
-			OCR0B = i / 262;
-		}
-		for (int i = 125 * 262; i >= 0; i--) {
-			OCR0B = i / 262;
-		}
-		for (int i = 1; i < 125 * 262; i++) {
-			OCR1B = i / 262;
-		}
-		for (int i = 125 * 262; i >= 0; i--) {
-			OCR1B = i / 262;
-		}
-		for (int i = 1; i < 125 * 262; i++) {
-			OCR2B = i / 262;
-		}
-		for (int i = 125 * 262; i >= 0; i--) {
-			OCR2B = i / 262;
-		}
 	}
 
 }
